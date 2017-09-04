7d5127b87d0a3b597df84d54e0a009aff2c61c54
googleknight <shubham.mathur2014@vit.ac.in>
2017-09-04 19:08:07 +0530

    Accessing Path params

--- messenger/src/main/java/learn/googleknight/rest/messenger/resources/MessageResource.java
+++ messenger/src/main/java/learn/googleknight/rest/messenger/resources/MessageResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -20,5 +21,13 @@ public class MessageResource {
 									// the type
 	public List<Message> getMessages() {
 		return messageservice.getAllMessages();
+	}
+	
+	@GET
+	@Path("/{messageId}") // By using {} it tells that part of url as variable and maps it to this method 
+	@Produces(MediaType.APPLICATION_XML)
+	public Message test(@PathParam("messageId")long messageId) // To use the value passed in url which is mapped to the variable 
+	{
+		return messageservice.getMessage(messageId);
 	}
 }
