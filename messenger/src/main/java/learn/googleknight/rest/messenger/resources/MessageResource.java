035f4646527bff405c73fe9820d39a206ad087c1
googleknight <shubham.mathur2014@vit.ac.in>
2017-09-04 08:17:19 +0530

    Returning a XML response

--- messenger/src/main/java/learn/googleknight/rest/messenger/resources/MessageResource.java
+++ messenger/src/main/java/learn/googleknight/rest/messenger/resources/MessageResource.java
@@ -1,16 +1,24 @@
 package learn.googleknight.rest.messenger.resources;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import learn.googleknight.rest.messenger.model.Message;
+import learn.googleknight.rest.messenger.service.MessageService;
+
 @Path("/messages") // To map message URI to this particular class
 public class MessageResource {
+
+	MessageService messageservice = new MessageService();
+
 	@GET // To map HTTP GET method to this particular method
-	@Produces(MediaType.TEXT_PLAIN) // To specify the response format // MediaType is a enumerator which specifies
+	@Produces(MediaType.APPLICATION_XML) // To specify the response format // MediaType is a enumerator which specifies
 									// the type
-	public String getMessages() {
-		return "Hello World!!!!";
+	public List<Message> getMessages() {
+		return messageservice.getAllMessages();
 	}
 }
