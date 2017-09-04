89f34082c27ec6d1b91cee53773b8c597bad16ff
googleknight <shubham.mathur2014@vit.ac.in>
2017-09-04 21:45:47 +0530

    Update and delete methods using PUT, DELETE added

--- messenger/src/main/java/learn/googleknight/rest/messenger/resources/MessageResource.java
+++ messenger/src/main/java/learn/googleknight/rest/messenger/resources/MessageResource.java
@@ -3,8 +3,10 @@ package learn.googleknight.rest.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,27 +19,46 @@ import learn.googleknight.rest.messenger.service.MessageService;
 public class MessageResource {
 
 	MessageService messageservice = new MessageService();
-	//Message Body writer converts message into JSON/XML which is added in dependencies
-	
+	// Message Body writer converts message into JSON/XML which is added in
+	// dependencies
+
 	@GET // To map HTTP GET method to this particular method
 	@Produces(MediaType.APPLICATION_JSON) // To specify the response format // MediaType is a enumerator which specifies
-									// the type
+	// the type
 	public List<Message> getMessages() {
 		return messageservice.getAllMessages();
 	}
-	
-	@POST //To map HTTP POST method to this particular method
-	@Consumes(MediaType.APPLICATION_JSON)//return type of POST method
-	@Produces(MediaType.APPLICATION_JSON)//input that it accepts
+
+	@POST // To map HTTP POST method to this particular method
+	@Consumes(MediaType.APPLICATION_JSON) // return type of POST method
+	@Produces(MediaType.APPLICATION_JSON) // input that it accepts
 	public Message addMessage(Message message) {
 		return messageservice.addMessage(message);
+	}
+
+	@PUT // To map HTTP PUT method to this particular method
+	@Path("/{messageId}") // By using {} it tells that part of url as variable and maps it to this method
+	@Consumes(MediaType.APPLICATION_JSON) // return type of POST method
+	@Produces(MediaType.APPLICATION_JSON) // input that it accepts
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
+		message.setId(messageId);
+		System.out.println(message.getId());
+		return messageservice.updateMessage(message);
+	}
+
+	@DELETE
+	@Path("/{messageId}") // By using {} it tells that part of url as variable and maps it to this method
+	@Produces(MediaType.APPLICATION_JSON) // input that it accepts
+	public void deleteMessage(@PathParam("messageId") long messageId) {
+		messageservice.removeMessage(messageId);
 	}
 
 	@GET
 	@Path("/{messageId}") // By using {} it tells that part of url as variable and maps it to this method
 	@Produces(MediaType.APPLICATION_JSON)
-	public Message test(@PathParam("messageId") long messageId) // To use the value passed in url which is mapped to the
-																// variable
+	public Message getMessage(@PathParam("messageId") long messageId) // To use the value passed in url which is mapped
+																		// to the
+	// variable
 	{// Can handle multiple params, with different type too even regex
 		return messageservice.getMessage(messageId);
 	}
